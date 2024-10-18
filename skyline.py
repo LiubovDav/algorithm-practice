98a73e9a356725fcffacfb11a1c0b1b61694c2c0
Liubov Dav <lubasha0512@gmail.com>
2024-10-17 23:56:19 -0400

    Implemented Skyline function

--- skyline.py
+++ skyline.py
@@ -1,4 +1,14 @@
 # Add your clarifying questions here
 
 def skyline(building_list):
-    pass
+    max_building_height = 0
+    result = []
+
+    for i in range(len(building_list) - 1):
+        if building_list[i] > max_building_height:
+            result.append(building_list[i])
+            max_building_height = building_list[i]
+
+    return result
+
+print(skyline([-1, 1, 3, 7, 7, 3]))
